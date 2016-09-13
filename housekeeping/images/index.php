e65cb0e675e86a53c0bbaeb54e3e937646ab117e
Miguel González Aravena <miguel.gonzalez.93@gmail.com>
2016-09-13 05:23:39 -0400

    Modificar archivo index de directorios, eliminar archivos basura

--- housekeeping/images/index.php
+++ housekeeping/images/index.php
@@ -2,23 +2,11 @@
 /*==================================+
 || # HoloCMS - Website and Content Management System
 |+==================================+
-|| # Copyright © 2016 Miguel González Aravena. All rights reserved.
+|| # Copyright Â© 2016 Miguel GonzÃ¡lez Aravena. All rights reserved.
 || # https://github.com/MiguelGonzalezAravena/HoloCMS
 |+==================================+
 || # HoloCMS is provided "as is" and comes without
 || # warrenty of any kind. HoloCMS is free software!
 |+==================================*/
-
-header("location:../index.php");
-
+header('Location: ../index.php');
 ?>
-
-<html>
-<head>
-  <title>Redirecting...</title>
-  <meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
-  <style type="text/css">body { background-color: #e3e3db; text-align: center; font: 11px Verdana, Arial, Helvetica, sans-serif; } a { color: #fc6204; }</style>
-</head>
-<body>
-</body>
-</html>
