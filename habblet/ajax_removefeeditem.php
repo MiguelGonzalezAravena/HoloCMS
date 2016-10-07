f9568aa4381781cba595fc3d1acac867ef75f2e6
Miguel González Aravena <miguel.gonzalez.93@gmail.com>
2016-10-07 19:59:01 -0400

    Comentar línea

--- habblet/ajax_removefeeditem.php
+++ habblet/ajax_removefeeditem.php
@@ -14,7 +14,7 @@ require_once(dirname(__FILE__) . '/../core.php');
 require_once(dirname(__FILE__) . '/../includes/session.php');
 
 $feedItemIndex = isset($_POST['feedItemIndex']) ? (int) $_POST['feedItemIndex'] : '';
-(empty($feedItemIndex)) ? exit : '';
+//(empty($feedItemIndex)) ? exit : '';
 mysqli_query($connection, "DELETE FROM cms_alerts WHERE userid = '{$my_id}' ORDER BY id ASC LIMIT 1") or die(mysqli_error($connection));
 echo 'SUCCESS';
 ?>
